6195cb06a68e29f4fe2ce9b46590f23e0b8ba01b
Paul Nykiel <paul.nykiel@gmail.com>
2021-11-15 10:02:56 +0100

    Fixed functions

--- Source/Symbolic/Operations/Functions/Functions.hpp
+++ Source/Symbolic/Operations/Functions/Functions.hpp
@@ -12,17 +12,17 @@
 #include "Tan.hpp"
 
 namespace std { // NOLINT everything is fine specialization for custom types is allowed.
-    template<grad::sym::Expression Expr>
+    template<sym::Expression Expr>
     auto sin(Expr expr) {
         return grad::sym::Sin<Expr>{expr};
     }
 
-    template<grad::sym::Expression Expr>
+    template<sym::Expression Expr>
     auto cos(Expr expr) {
         return grad::sym::Cos<Expr>{expr};
     }
 
-    template<grad::sym::Expression Expr>
+    template<sym::Expression Expr>
     auto tan(Expr expr) {
         return grad::sym::Tan<Expr>{expr};
     }
