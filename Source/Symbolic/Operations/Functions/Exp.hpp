cebeafd6e342504c7a38ce26047424e5b2394130
Paul Nykiel <paul.nykiel@gmail.com>
2021-11-16 14:39:02 +0100

    Added exp

---
+++ Source/Symbolic/Operations/Functions/Exp.hpp
@@ -0,0 +1,56 @@
+/**
+ * @file Exp.hpp
+ * @author paul
+ * @date 21.10.20
+ * Description here TODO
+ */
+#ifndef SYM_COS_HPP
+#define SYM_COS_HPP
+
+#include <cmath>
+
+#include "../../Expression.hpp"
+#include "../../Variable.hpp"
+#include "../Mul.hpp"
+
+namespace sym {
+    template<Expression Expr>
+    class Exp {
+      public:
+        explicit Exp(Expr expr);
+
+        template<typename... Bindings>
+        constexpr auto resolve(Bindings... bindings) const;
+
+        template<Expression Expr_, std::size_t ID>
+        constexpr friend auto gradient(const Exp<Expr_> &x, const Variable<ID> &d);
+
+        template<Expression Expr_>
+        friend auto toString(const Exp<Expr_> &x) -> std::string;
+
+      private:
+        Expr expr;
+    };
+
+    template<Expression Expr>
+    Exp<Expr>::Exp(Expr expr) : expr{expr} {
+    }
+
+    template<Expression Expr>
+    template<typename... Bindings>
+    constexpr auto Exp<Expr>::resolve(Bindings... bindings) const {
+        return std::exp(expr.resolve(bindings...));
+    }
+
+    template<Expression Expr_, std::size_t ID>
+    constexpr auto gradient(const Exp<Expr_> &x, const Variable<ID> &d) {
+        return Mul{Exp{x.expr}, gradient(x.expr, d)};
+    }
+
+    template<Expression Expr_>
+    auto toString(const Exp<Expr_> &x) -> std::string {
+        return "exp(" + toString(x.expr) + ")";
+    }
+} // namespace sym
+
+#endif // SYM_COS_HPP
---
+++ Test/Symbolic/Operations/Functions/Exp.cpp
@@ -0,0 +1,23 @@
+#include "Symbolic/Operations/Functions/Exp.hpp"
+
+#include <cmath>
+#include <gtest/gtest.h>
+
+#include "Symbolic/RuntimeConstant.hpp"
+
+TEST(Exp, Expression) {
+    using Const = sym::RuntimeConstant<int>;
+    EXPECT_TRUE((sym::IsExpression<sym::Exp<Const>>::val));
+}
+
+TEST(Exp, Resolve) {
+    sym::RuntimeConstant<double> a{17};
+    sym::Exp<decltype(a)> exp{a};
+    EXPECT_DOUBLE_EQ(exp.resolve(), std::exp(17));
+}
+
+TEST(Exp, Gradient) {
+    sym::Variable<'x'> x;
+    sym::Exp<decltype(x)> y{x};
+    EXPECT_DOUBLE_EQ(sym::gradient(y, x).resolve(x=1), std::exp(1));
+}
