ca12da8ae9496b5dd8c66e5260189ea68e2eee99
Paul Nykiel <paul.nykiel@gmail.com>
2021-11-16 14:03:44 +0100

    Added function

--- Source/Symbolic/Operations/Functions/Functions.hpp
+++ Source/Symbolic/Operations/Functions/Functions.hpp
@@ -10,6 +10,7 @@
 #include "Cos.hpp"
 #include "Sin.hpp"
 #include "Tan.hpp"
+#include "Sqrt.hpp"
 
 namespace std { // NOLINT everything is fine specialization for custom types is allowed.
     template<sym::Expression Expr>
@@ -25,6 +26,11 @@ namespace std { // NOLINT everything is fine specialization for custom types is 
     template<sym::Expression Expr>
     auto tan(Expr expr) {
         return sym::Tan<Expr>{expr};
+    }
+
+    template<sym::Expression Expr>
+    auto sqrt(Expr expr) {
+        return sym::Sqrt<Expr>{expr};
     }
 } // namespace std
 
