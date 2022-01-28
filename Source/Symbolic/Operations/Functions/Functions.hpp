5b051288631bf9f42c9ed3c3f64b41b9fe897ffb
Jonas Otto <jottosmail@gmail.com>
2022-01-28 12:27:36 +0100

    create Func<remove_reference<T>> from std::Func<T>

--- Source/Symbolic/Operations/Functions/Functions.hpp
+++ Source/Symbolic/Operations/Functions/Functions.hpp
@@ -23,98 +23,112 @@
 namespace std { // NOLINT everything is fine specialization for custom types is allowed.
     template<sym::Expression Expr>
     auto sin(Expr &&expr) {
-        return sym::Sin<Expr>{std::forward<Expr>(expr)};
+        return sym::Sin<std::remove_reference_t<Expr>>{std::forward<Expr>(expr)};
     }
 
     template<sym::Expression Expr>
     auto cos(Expr &&expr) {
-        return sym::Cos<Expr>{std::forward<Expr>(expr)};
+        return sym::Cos<std::remove_reference_t<Expr>>{std::forward<Expr>(expr)};
     }
 
     template<sym::Expression Expr>
     auto tan(Expr &&expr) {
-        return sym::Tan<Expr>{std::forward<Expr>(expr)};
+        return sym::Tan<std::remove_reference_t<Expr>>{std::forward<Expr>(expr)};
     }
 
     template<sym::Expression Expr>
     auto sqrt(Expr &&expr) {
-        return sym::Sqrt<Expr>{std::forward<Expr>(expr)};
+        return sym::Sqrt<std::remove_reference_t<Expr>>{std::forward<Expr>(expr)};
     }
 
     template<sym::Expression Expr>
     auto exp(Expr &&expr) {
-        return sym::Exp<Expr>{std::forward<Expr>(expr)};
+        return sym::Exp<std::remove_reference_t<Expr>>{std::forward<Expr>(expr)};
     }
 
     template<sym::Expression Expr>
     auto asin(Expr &&expr) {
-        return sym::ArcSin{std::forward<Expr>(expr)};
+        return sym::ArcSin<std::remove_reference_t<Expr>>{std::forward<Expr>(expr)};
     }
 
     template<sym::Expression Expr>
     auto acos(Expr &&expr) {
-        return sym::ArcCos{std::forward<Expr>(expr)};
+        return sym::ArcCos<std::remove_reference_t<Expr>>{std::forward<Expr>(expr)};
     }
 
     template<sym::Expression Expr>
     auto atan(Expr &&expr) {
-        return sym::ArcTan{std::forward<Expr>(expr)};
+        return sym::ArcTan<std::remove_reference_t<Expr>>{std::forward<Expr>(expr)};
     }
 
     template<sym::Expression Expr>
     auto log(Expr &&expr) {
-        return sym::Log{std::forward<Expr>(expr)};
+        return sym::Log<std::remove_reference_t<Expr>>{std::forward<Expr>(expr)};
     }
 
     template<sym::Expression Base, sym::Expression Exp>
     auto pow(Base &&base, Exp &&exp) {
-        return sym::Pow{std::forward<Base>(base), std::forward<Exp>(exp)};
+        return sym::Pow<std::remove_reference_t<Base>, std::remove_reference_t<Exp>>{std::forward<Base>(base),
+                                                                                     std::forward<Exp>(exp)};
     }
 
     template<typename Base, sym::Expression Exp>
     auto pow(Base &&base, Exp &&exp) {
-        return sym::Pow{sym::RuntimeConstant{std::forward<Base>(base)}, std::forward<Exp>(exp)};
+        return sym::Pow<std::remove_reference_t<Base>, std::remove_reference_t<Exp>>{
+                sym::RuntimeConstant{std::forward<Base>(base)}, std::forward<Exp>(exp)};
     }
 
     template<sym::Expression Base, typename Exp>
     auto pow(Base &&base, Exp &&exp) {
-        return sym::Pow{std::forward<Base>(base), sym::RuntimeConstant{std::forward<Exp>(exp)}};
+        return sym::Pow<std::remove_reference_t<Base>, std::remove_reference_t<Exp>>{
+                std::forward<Base>(base), sym::RuntimeConstant{std::forward<Exp>(exp)}};
     }
 
     // Ternary based
     template<sym::Expression Lhs, sym::Expression Rhs>
     auto max(const Lhs &lhs, const Rhs &rhs) {
-        return sym::Ternary{lhs > rhs, lhs, rhs};
+        return sym::Ternary<decltype(lhs > rhs), std::remove_reference_t<Lhs>, std::remove_reference_t<Rhs>>{lhs > rhs,
+                                                                                                             lhs, rhs};
     }
 
     template<sym::Expression Lhs, sym::Expression Rhs>
     auto min(const Lhs &lhs, const Rhs &rhs) {
-        return sym::Ternary{lhs < rhs, lhs, rhs};
+        return sym::Ternary<std::remove_reference_t<decltype(lhs < rhs)>, std::remove_reference_t<Lhs>,
+                            std::remove_reference_t<Rhs>>{lhs < rhs, lhs, rhs};
     }
 
     template<sym::Expression Lhs, typename Rhs>
     auto max(const Lhs &lhs, const Rhs &rhs) {
-        return sym::Ternary{lhs > sym::RuntimeConstant{rhs}, lhs, sym::RuntimeConstant{rhs}};
+        return sym::Ternary<std::remove_reference_t<decltype(lhs > sym::RuntimeConstant{rhs})>,
+                            std::remove_reference_t<Lhs>, std::remove_reference_t<Rhs>>{lhs > sym::RuntimeConstant{rhs},
+                                                                                        lhs, sym::RuntimeConstant{rhs}};
     }
 
     template<sym::Expression Lhs, typename Rhs>
     auto min(const Lhs &lhs, const Rhs &rhs) {
-        return sym::Ternary{lhs < sym::RuntimeConstant{rhs}, lhs, sym::RuntimeConstant{rhs}};
+        return sym::Ternary<std::remove_reference_t<decltype(lhs < sym::RuntimeConstant{rhs})>,
+                            std::remove_reference_t<Lhs>, std::remove_reference_t<Rhs>>{lhs < sym::RuntimeConstant{rhs},
+                                                                                        lhs, sym::RuntimeConstant{rhs}};
     }
 
     template<typename Lhs, sym::Expression Rhs>
     auto max(const Lhs &lhs, const Rhs &rhs) {
-        return sym::Ternary{sym::RuntimeConstant{lhs} > rhs, sym::RuntimeConstant{lhs}, rhs};
+        return sym::Ternary<std::remove_reference_t<decltype(sym::RuntimeConstant{lhs} > rhs)>,
+                            std::remove_reference_t<Lhs>, std::remove_reference_t<Rhs>>{sym::RuntimeConstant{lhs} > rhs,
+                                                                                        sym::RuntimeConstant{lhs}, rhs};
     }
 
     template<typename Lhs, sym::Expression Rhs>
     auto min(const Lhs &lhs, const Rhs &rhs) {
-        return sym::Ternary{sym::RuntimeConstant{lhs} < rhs, sym::RuntimeConstant{lhs}, rhs};
+        return sym::Ternary<
+                std::remove_reference_t<decltype(sym::RuntimeConstant{lhs} < rhs, sym::RuntimeConstant{lhs})>,
+                std::remove_reference_t<Lhs>, std::remove_reference_t<Rhs>>{sym::RuntimeConstant{lhs} < rhs,
+                                                                            sym::RuntimeConstant{lhs}, rhs};
     }
 
     template<sym::Expression Expr>
     auto abs(const Expr &expr) {
-        return std::max(expr, -expr);
+        return std::max<std::remove_reference_t<Expr>>(expr, -expr);
     }
 } // namespace std
 
